1181970acfc6f13ccc401e153c3d2aafc1142f4c
Carlos Arturo Miranda Trujano <arturo.miranda@CDMX.PTH>
2023-06-26 17:44:03 -0600

    ID of very article

--- src/views/Account/GetData.jsx
+++ src/views/Account/GetData.jsx
@@ -7,12 +7,13 @@ import firebaseConfig from "../../config/firebase-config";
 import { initializeApp } from "firebase/app";
 import { useEffect, useState } from "react";
 import { CardArticle } from "../../components/Card/CardArticle";
-import { useParams } from "react-router-dom";
+import { Link, NavLink, useParams } from "react-router-dom";
 // import CardArticle from "../../components/Card/CardArticle";
 
 function GetData() {
 	const [data, setData] = useState([]);
-	const { articleId } = useParams();
+	// const [key, setKey] = useState('');
+	// const { articleId } = useParams();
 
 	const app = initializeApp(firebaseConfig);
 	const db = getFirestore(app);
@@ -20,28 +21,45 @@ function GetData() {
 
 	useEffect(() => {
 		test();
-	}, [articleId]);
+	}, []);
 
 	const test = async () => {
 		const arr = [];
 		const data = await getDocs(docRef);
 		data.forEach((item) => {
-			arr.push(item.data());
-			// arr.reverse().push(item.data());
+			arr.push(item);
 		});
 		setData(arr);
 	}
 
+
 	return (
-		<div className="flex justify-center flex-wrap text-gray-50 pb-28 my-20">
+		<div className="flex justify-center flex-wrap text-gray-50 pb-28 mt-20">
 			{data.slice(0, 6).map((item) => {
 				return (
-					<CardArticle data={item} key={item.articleTitle}>
-						<button>See more...</button>
+					<CardArticle data={item.data()} key={item.id}>
+						<Link to={item.id}>
+							<button className="bg-[#ffffff17] text-white font-medium px-4 py-2 rounded-md flex gap-1 items-center hover:bg-[#ffffff30] hover:animate-pulse">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-4 w-4"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									strokeWidth="2"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M13 7l5 5m0 0l-5 5m5-5H6"
+									/>
+								</svg>
+							</button>
+						</Link>
 					</CardArticle>
 				);
 			})}
-		</div>
+		</div >
 	);
 }
 
